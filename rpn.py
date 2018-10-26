8344c433664d26d086b1f394a519999224a68620
kriwang <kriwang@umich.edu>
2018-10-26 15:01:21 -0400

    Implement add (+).

--- rpn.py
+++ rpn.py
@@ -1,11 +1,26 @@
 # python3 rpn.py
 
 def calculate(arg):
-    pass
+	stack = []
+
+	tokens = arg.split()
+
+	for token in tokens:
+		try:
+			stack.append(int(token))
+		except ValueError:
+			val1 = stack.pop()
+			val2 = stack.pop()
+			result = val1 + val2
+
+			stack.append(result)
+
+	return stack[0]
 
 def main():
-    while True:
-        calculate(input("rpn calc> "))
+	while True:
+		result = calculate(input("rpn calc> "))
+		print(result)
 
 if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
-    main()
+	main()
