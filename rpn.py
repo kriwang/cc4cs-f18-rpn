853420ed59e1874d39c07960f9233cdbde2334f5
kriwang <kriwang@umich.edu>
2018-10-26 14:44:11 -0400

    Initial Commit

---
+++ rpn.py
@@ -0,0 +1,11 @@
+# python3 rpn.py
+
+def calculate(arg):
+    pass
+
+def main():
+    while True:
+        calculate(input("rpn calc> "))
+
+if __name__ == '__main__': # Note: that's "underscore underscore n a m e ..."
+    main()
